6c06b27268cf5b4d21e3f8b7a34d7a53cc87b0c3
Xiquinho32 <franciscofcp32@gmail.com>
2022-06-01 19:07:20 +0100

    função criada para inserir valores no TabelaBD.kt

--- app/src/main/java/pt/ipg/projeto1/TabelaBD.kt
+++ app/src/main/java/pt/ipg/projeto1/TabelaBD.kt
@@ -1,4 +1,13 @@
 package pt.ipg.projeto1
 
-class TabelaBD {
+import android.content.ContentValues
+import android.database.sqlite.SQLiteDatabase
+
+abstract class TabelaBD(val db: SQLiteDatabase, val nome: String) {
+    abstract fun cria()
+
+    fun insert(values: ContentValues) =
+        db.insert(nome, null, values)
+
+
 }
