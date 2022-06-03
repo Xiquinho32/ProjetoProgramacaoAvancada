25f4d8bf47c6941fe35bb42f4b2ef426f99228cb
Xiquinho32 <franciscofcp32@gmail.com>
2022-06-03 09:48:59 +0100

    criada a fun cria para a TabelaBDMedicos.kt com companion object

--- app/src/main/java/pt/ipg/projeto1/TabelaBDMedicos.kt
+++ app/src/main/java/pt/ipg/projeto1/TabelaBDMedicos.kt
@@ -1,4 +1,16 @@
 package pt.ipg.projeto1
 
-class TabelaBDMedicos {
+import android.database.sqlite.SQLiteDatabase
+import android.provider.BaseColumns
+
+class TabelaBDMedicos(db: SQLiteDatabase) : TabelaBD(db, NOME) {
+    override fun cria() {
+        db.execSQL("CREATE TABLE ${TabelaBDMedicos.Campo_ID_medico} (${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT, ${TabelaBDMedicos.Campo_CC} LONG NOT NULL, ${TabelaBDMedicos.Campo_Especialidades} STRING NOT NULL)")  }
+
+    companion object{
+        const val NOME = "Médicos"
+        const val Campo_ID_medico = "id_medico"
+        const val Campo_CC = "cartão_cidadão"
+        const val Campo_Especialidades = "especialidades"
+    }
 }
