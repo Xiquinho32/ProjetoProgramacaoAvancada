27d4813cf9c5fb1b611c422a0844b6dfd4d8a8b0
Xiquinho32 <franciscofcp32@gmail.com>
2022-06-11 20:10:08 +0100

    inseridos valores na data class do Consultas.kt

--- app/src/main/java/pt/ipg/projeto1/Consultas.kt
+++ app/src/main/java/pt/ipg/projeto1/Consultas.kt
@@ -1,7 +1,16 @@
 package pt.ipg.projeto1
 
-import java.sql.Date
+import android.content.ContentValues
 
-data class Consultas(
-    var Data: Date //java.sql
-) {}
+data class Consultas(var nome: String, var cc: String, var doente_id : Long = -1, var medico_id : Long = -1, var id: Long = -1){
+    fun toContentValues() : ContentValues{
+        val valores =ContentValues()
+
+        valores.put(TabelaBDConsultas.NOME, nome)
+        valores.put(TabelaBDConsultas.Campo_CC, cc)
+        valores.put(TabelaBDConsultas.Campo_Doente_ID, doente_id)
+        valores.put(TabelaBDConsultas.Campo_Medico_ID, medico_id)
+
+        return valores;
+    }
+}
