1537a86e29af204206534858ef627de2f350b17e
Xiquinho32 <franciscofcp32@gmail.com>
2022-06-18 19:25:26 +0100

    criada a TabelaBDEspecialidades.kt e inserido as variaveis na Especialidades.kt

--- app/src/main/java/pt/ipg/projeto1/Especialidades.kt
+++ app/src/main/java/pt/ipg/projeto1/Especialidades.kt
@@ -1,4 +1,10 @@
 package pt.ipg.projeto1
 
-class Especialidades {
+data class Especialidades(
+    var NOME : String,
+    var Acupuntura : String,
+    var Cardiologia : String,
+    var Cirugia_Geral : String,
+    var Anestesiologia : String
+) {
 }
