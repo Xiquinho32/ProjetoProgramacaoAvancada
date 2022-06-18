06f96a3d004b9d0f73fb9ec1628d9ebfedf46633
Xiquinho32 <franciscofcp32@gmail.com>
2022-06-18 19:33:10 +0100

    criada overide fun para criar a base de dados e inserido o companion object

--- app/src/main/java/pt/ipg/projeto1/TabelaBDEspecialidades.kt
+++ app/src/main/java/pt/ipg/projeto1/TabelaBDEspecialidades.kt
@@ -1,4 +1,17 @@
 package pt.ipg.projeto1
 
-class TabelaBDEspecialidades {
+import android.database.sqlite.SQLiteDatabase
+import android.provider.BaseColumns
+
+class TabelaBDEspecialidades(db: SQLiteDatabase) : TabelaBD(db, NOME) {
+    override fun cria() {
+        db.execSQL("CREATE TABLE $NOME (${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT, $Campo_Acupuntura TEXT NOT NULL, $Campo_Cirugia_Geral TEXT NOT NULL, $Campo_Anestesiologia TEXT NOT NULL, $Campo_Cardiologia TEXT NOT NULL)")
+    }
+    companion object{
+        const val NOME = "Especialidades"
+        const val Campo_Acupuntura ="Acupuntura"
+        const val Campo_Cardiologia = "Cardiologia"
+        const val Campo_Cirugia_Geral = "Cirugia_Geral"
+        const val Campo_Anestesiologia ="Anestesiologia"
+    }
 }
